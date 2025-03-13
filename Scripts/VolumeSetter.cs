81baa5b1111666a022c6051fbb783412b4d4c63e
Chips-Wincheser <gleba.trigub@mail.ru>
2025-03-13 12:38:19 +0700

    fix

---
+++ Scripts/VolumeSetter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSetter : MonoBehaviour
+{
+    private const string BackgroundMusic = "BackgroundMusic";
+    private const string ButtonMusic = "ButtonMusic";
+    private const string MasterMusic = "MasterMusic";
+
+    [SerializeField] private AudioMixer _masterMixer;
+
+    [SerializeField] private Slider _volumeSlider;
+
+    private float _musicLvl;
+
+    public void SetVolume(string mixerName)
+    {
+        _musicLvl = _volumeSlider.value;
+        
+        if (mixerName==MasterMusic)
+        {
+            _masterMixer.SetFloat(mixerName, Mathf.Log10(_musicLvl) * 20);
+        }
+        else if (mixerName==ButtonMusic)
+        {
+            _masterMixer.SetFloat(mixerName, Mathf.Log10(_musicLvl) * 20);
+        }
+        else if (mixerName==BackgroundMusic)
+        {
+            _masterMixer.SetFloat(mixerName, Mathf.Log10(_musicLvl) * 20);
+        }
+    }
+}
